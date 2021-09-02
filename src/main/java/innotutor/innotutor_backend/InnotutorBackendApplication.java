40b34db8faff3c52b2d8ef6a6b1bf610260c9562
Daniil <danya_livitin@mail.ru>
2021-09-02 13:12:55 +0300

    Created initial project structure. To run it, you need to specify the application.properties in the resources folder

---
+++ src/main/java/innotutor/innotutor_backend/InnotutorBackendApplication.java
@@ -0,0 +1,13 @@
+package innotutor.innotutor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnotutorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnotutorBackendApplication.class, args);
+	}
+
+}
