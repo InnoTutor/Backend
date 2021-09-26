26c0d3bd2974e168bafc5843810ed8f6180a828f
SMore-Napi <s.roma245@gmail.com>
2021-09-26 12:19:27 +0300

    Changed data type from String to Time and Date for Session attributes #3

--- src/main/java/innotutor/innotutor_backend/entity/Session.java
+++ src/main/java/innotutor/innotutor_backend/entity/Session.java
@@ -1,6 +1,31 @@
+/*
+MIT License
+
+Copyright (c) 2021 InnoTutor
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
 package innotutor.innotutor_backend.entity;
 
 import javax.persistence.*;
+import java.sql.Date;
+import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.Collection;
 
@@ -11,9 +36,9 @@ public class Session {
     private Long subjectId;
     private Long sessionFormatId;
     private Long sessionTypeId;
-    private String date;
-    private String startTime;
-    private String endTime;
+    private Date date;
+    private Time startTime;
+    private Time endTime;
     private String description;
     private Timestamp creationDate;
     private Timestamp lastUpdate;
@@ -74,32 +99,32 @@ public class Session {
     }
 
     @Basic
-    @Column(name = "date", nullable = false, length = 256)
-    public String getDate() {
+    @Column(name = "date", nullable = false)
+    public Date getDate() {
         return date;
     }
 
-    public void setDate(String date) {
+    public void setDate(Date date) {
         this.date = date;
     }
 
     @Basic
-    @Column(name = "start_time", nullable = false, length = 256)
-    public String getStartTime() {
+    @Column(name = "start_time", nullable = false)
+    public Time getStartTime() {
         return startTime;
     }
 
-    public void setStartTime(String startTime) {
+    public void setStartTime(Time startTime) {
         this.startTime = startTime;
     }
 
     @Basic
-    @Column(name = "end_time", nullable = false, length = 256)
-    public String getEndTime() {
+    @Column(name = "end_time", nullable = false)
+    public Time getEndTime() {
         return endTime;
     }
 
-    public void setEndTime(String endTime) {
+    public void setEndTime(Time endTime) {
         this.endTime = endTime;
     }
 
