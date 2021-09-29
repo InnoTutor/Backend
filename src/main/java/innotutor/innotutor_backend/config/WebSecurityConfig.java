5a99a15049d426f71e7d8b3cec46920da69056d2
SMore-Napi <s.roma245@gmail.com>
2021-09-29 14:30:14 +0300

    Update WebSecurityConfig.java

--- src/main/java/innotutor/innotutor_backend/config/WebSecurityConfig.java
+++ src/main/java/innotutor/innotutor_backend/config/WebSecurityConfig.java
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
-package innotutor.innotutor_backend;
+package innotutor.innotutor_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -52,5 +52,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
 }
