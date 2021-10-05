941821d277f425667dfecfdb47c3a20339b87a6e
SMore-Napi <s.roma245@gmail.com>
2021-10-05 23:56:46 +0300

    Deployment...

--- src/main/java/innotutor/innotutor_backend/InnotutorBackendApplication.java
+++ src/main/java/innotutor/innotutor_backend/InnotutorBackendApplication.java
@@ -27,6 +27,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.json.JSONObject;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -37,14 +38,16 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @SpringBootApplication
 public class InnotutorBackendApplication {
 
-    @Value("${firebase.credential.resource-path}")
-    private String keyPath;
+//    @Value("${firebase.credential.resource-path}")
+//    private String keyPath;
 
     public static void main(String[] args) {
         SpringApplication.run(InnotutorBackendApplication.class, args);
@@ -55,18 +58,43 @@ public class InnotutorBackendApplication {
         return new ModelMapper();
     }
 
+    @Value("${firebase.path}")
+    private String path;
+
+    @Value("${firebase.database-url}")
+    private String databaseUrl;
+
+    @Value("${firebase.storage-url}")
+    private String storageUrl;
+
+    @Value("${GOOGLE_CREDENTIALS}")
+    private String gservicesConfig;
+
     @Bean
-    @Primary
-    public void firebaseInitialization() throws IOException {
-        Resource resource = new ClassPathResource(keyPath);
-        FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+    public FirebaseApp provideFirebaseOptions() throws IOException {
+        JSONObject jsonObject = new JSONObject(gservicesConfig);
+        InputStream is = new ByteArrayInputStream(jsonObject.toString().getBytes());
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream((is)))
+                .setDatabaseUrl(databaseUrl)
+                .setStorageBucket(storageUrl)
                 .build();
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
-        }
+
+        return FirebaseApp.initializeApp(options);
     }
+
+//    @Bean
+//    @Primary
+//    public void firebaseInitialization() throws IOException {
+//        Resource resource = new ClassPathResource(keyPath);
+//        FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+//        FirebaseOptions options = new FirebaseOptions.Builder()
+//                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                .build();
+//        if (FirebaseApp.getApps().isEmpty()) {
+//            FirebaseApp.initializeApp(options);
+//        }
+//    }
 
     @Bean
     @Primary
