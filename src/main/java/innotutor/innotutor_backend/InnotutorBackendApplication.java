0e76966ae36ea65306d320b8dac481ec7192263f
SMore-Napi <s.roma245@gmail.com>
2021-10-06 00:13:45 +0300

    Deploy

--- src/main/java/innotutor/innotutor_backend/InnotutorBackendApplication.java
+++ src/main/java/innotutor/innotutor_backend/InnotutorBackendApplication.java
@@ -58,14 +58,30 @@ public class InnotutorBackendApplication {
         return new ModelMapper();
     }
 
-    @Value("${firebase.path}")
-    private String path;
-
-    @Value("${firebase.database-url}")
-    private String databaseUrl;
-
-    @Value("${firebase.storage-url}")
-    private String storageUrl;
+//    @Value("${firebase.path}")
+//    private String path;
+//
+//    @Value("${firebase.database-url}")
+//    private String databaseUrl;
+//
+//    @Value("${firebase.storage-url}")
+//    private String storageUrl;
+//
+//    @Value("${GOOGLE_CREDENTIALS}")
+//    private String gservicesConfig;
+//
+//    @Bean
+//    public FirebaseApp provideFirebaseOptions() throws IOException {
+//        JSONObject jsonObject = new JSONObject(gservicesConfig);
+//        InputStream is = new ByteArrayInputStream(jsonObject.toString().getBytes());
+//        FirebaseOptions options = new FirebaseOptions.Builder()
+//                .setCredentials(GoogleCredentials.fromStream((is)))
+//                .setDatabaseUrl(databaseUrl)
+//                .setStorageBucket(storageUrl)
+//                .build();
+//
+//        return FirebaseApp.initializeApp(options);
+//    }
 
     @Value("${GOOGLE_CREDENTIALS}")
     private String gservicesConfig;
@@ -76,8 +92,6 @@ public class InnotutorBackendApplication {
         InputStream is = new ByteArrayInputStream(jsonObject.toString().getBytes());
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream((is)))
-                .setDatabaseUrl(databaseUrl)
-                .setStorageBucket(storageUrl)
                 .build();
 
         return FirebaseApp.initializeApp(options);
